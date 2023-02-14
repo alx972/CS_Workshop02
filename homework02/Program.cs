24f5ff9de7d065ea5e4ea28da4ef7618153e59db
Alex Slyussarev <alex.slyussarev@yandex.ru>
2023-02-14 23:54:30 +0300

    Add homework02 tiny program

--- homework02/Program.cs
+++ homework02/Program.cs
@@ -1,2 +1,23 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа 
 // или сообщает, что третьей цифры нет. Не использовать строки для расчета.
+int Prompt(string msg)
+{
+    System.Console.Write($"{msg} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Prompt ("Введите любое целое число");
+int result = Math.Abs(number);
+
+if (result > 100)
+{
+    while (result > 1000)
+    {
+        result /= 10;
+    }
+    System.Console.WriteLine($"{number} -> третья цифра {result % 10}");
+}
+else
+{
+    System.Console.WriteLine($"{number} -> третьей цифры нет");
+}
